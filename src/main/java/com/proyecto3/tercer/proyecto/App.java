5db99a0171da393f04b9cbff6e2f85c7507ca90e
117CarlosCoder <carloslopez202031871@cunoc.edu.gt>
2022-05-26 12:03:18 -0600

    Primer commit

---
+++ src/main/java/com/proyecto3/tercer/proyecto/App.java
@@ -0,0 +1,13 @@
+package com.proyecto3.tercer.proyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
